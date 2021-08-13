a7c75d8510817713e1c156b09536730144a0450f
coyad45 <coyad45@gmail.com>
2021-08-13 15:57:39 -0300

    Primer commit: Commit completado del codigo

---
+++ SolucionM/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolucionM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] separacion;
+            Console.WriteLine("Ingrese una palabra o oracion para volverla en su contra parte espejo");
+            string palabra = Console.ReadLine();
+            char[] caracterArray = palabra.ToCharArray();
+            Array.Reverse(caracterArray);
+
+            string union = new string(caracterArray);
+            separacion = union.Split(' ');
+            Array.Reverse(separacion);
+
+            
+            for (int i = 0; i < separacion.Length; i++)
+            {
+                Console.Write(separacion[i]+" ");
+            }
+            Console.ReadKey();
+            Console.WriteLine("");
+            
+        
+
+
+            
+
+
+
+
+        }
+    }
+}
